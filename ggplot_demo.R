763d7692e5100a30a14955212392edfbf27e683a
Leonardo Barozzi <leonardo.barozzi@gmail.com>
2025-11-06 12:16:27 +0100

    update esercizio insieme

--- ggplot_demo.R
+++ ggplot_demo.R
@@ -11,4 +11,5 @@ ggplot(data=mpg) + geom_point(mapping= aes(x=displ, y=hwy, color=class)) + geom_
 ggplot(data=mpg) + geom_point(mapping= aes(x=displ, y=hwy, color=class)) + geom_smooth(mapping= aes(x=displ, y=hwy, linetype=drv))
 ggplot(data=mpg) + geom_point(mapping= aes(x=displ, y=hwy, color=class)) + geom_smooth(mapping= aes(x=displ, y=hwy))
 ggplot(data=mpg, mapping=aes(x=displ, y=hwy)) + geom_point(mapping= aes(color=class)) + geom_smooth(data=filter(mpg, class=="compact"), se = FALSE )
-
+#Laboratorio
+ggplot(data=mpg) + geom_bar(mapping=aes(x=class,fill=class))
